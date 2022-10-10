b535d06db70f9efa8198425b19a8bc36f3cd3268
IvanK <mr.koldamasov@mail.ru>
2022-10-10 15:11:20 +0300

    Выевляет чётное число илии нет

---
+++ HomeTask06/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int num = n % 2;
+
+if (num == 0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
