f2d554090ef231372cff802893edf999e1140b05
IvanK <mr.koldamasov@mail.ru>
2022-10-10 22:53:32 +0300

    Поиск максимального числа из 3х чисел

---
+++ HomeTask04/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число a: ");
+Console.WriteLine("Введите число b: ");
+Console.WriteLine("Введите число c: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (a > b)
+{
+    max = a;
+}
+else 
+{
+    max = b;
+}
+if (max > c)
+{
+    max = max;
+}
+else 
+{
+    max = c;
+}
+Console.WriteLine(max);
