082d3162cdbb55af9cd663bfd690d00fdcd39ca7
IvanK <mr.koldamasov@mail.ru>
2022-10-10 22:55:28 +0300

    Поис максимального числа из 2х чисел

---
+++ HomeTask02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число a: ");
+Console.WriteLine("Введите число b: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max = {0}", a);
+    Console.WriteLine("min = {0}", b);
+}
+else 
+{
+    Console.WriteLine("max = {0}", b);
+    Console.WriteLine("min = {0}", a);
+}
